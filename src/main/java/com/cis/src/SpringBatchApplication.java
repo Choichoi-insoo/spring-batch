53e997a0b1117ec7598c1c7219113cd7a3583739
modeal_cis <129710115+modeal-cis@users.noreply.github.com>
2023-05-29 15:42:55 +0900

    스테이지 푸쉬

--- src/main/java/com/cis/src/SpringBatchApplication.java
+++ src/main/java/com/cis/src/SpringBatchApplication.java
@@ -11,7 +11,7 @@ public class SpringBatchApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBatchApplication.class); 
 
 	public static void main(String[] args) {
-		System.err.println("테스트에서 메인 병합에 대한 이벤트1");
+		System.err.println("스테이지에서 메인 병합에 대한 이벤트11");
 		SpringApplication.run(SpringBatchApplication.class, args);
 	}
 
